236ecbb75d32fa9b7a992607ebb6886f5251a0a5
ra402221 <ra402221@uaeh.edu.mx>
2026-05-06 17:34:56 -0600

    Update api.js

--- Server/api.js
+++ Server/api.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
