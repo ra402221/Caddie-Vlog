f00c26b909fd9dc1dfffb8e3aaec03bcb9bc4677
ra402221 <ra402221@uaeh.edu.mx>
2026-05-06 17:39:10 -0600

    Update api.js

--- Server/api.js
+++ Server/api.js
@@ -86,6 +86,10 @@ async function inicializarProductos() {
     }
 }
 
+app.get('/', (req, res) => {
+    res.send('Servidor funcionando. API de productos disponible en /api/productos');
+});
+
 // ========== ENDPOINTS CRUD ==========
 
 // GET - Obtener todos
